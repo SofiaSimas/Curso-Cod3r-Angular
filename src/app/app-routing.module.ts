8d5dba0bd5a8ff5f3262ac072ca6475433043d11
Sofia <sofiasimas1992@gmail.com>
2020-10-02 08:55:37 -0300

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './viwes/home/home.component';
+import { ProductCrudComponent } from './viwes/product-crud/product-crud.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+
+
+const routes: Routes = [
+  {
+    path:"",
+    component: HomeComponent
+  },
+  {
+    path:"products",
+    component: ProductCrudComponent
+  },
+  {
+    path:"products/create",
+    component: ProductCreateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
