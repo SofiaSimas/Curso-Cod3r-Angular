3bddaa3fcc4292168a62819513c61817b654b7f7
Sofia <sofiasimas1992@gmail.com>
2020-10-06 15:15:18 -0300

    routing modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './viwes/home/home.component';
 import { ProductCrudComponent } from './viwes/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
-
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 const routes: Routes = [
   {
@@ -18,6 +19,14 @@ const routes: Routes = [
   {
     path:"products/create",
     component: ProductCreateComponent
+  },
+  {
+    path:"products/update/:id",
+    component: ProductUpdateComponent
+  },
+  {
+    path:"products/delete/:id",
+    component: ProductDeleteComponent
   }
 ];
 
